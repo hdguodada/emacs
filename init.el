66574c26aa7eedb7a90f33aa0bc5a5a101be495f
yumo <261731963@qq.com>
2017-08-22 14:15:47 +0800

    config

--- init.el
+++ init.el
@@ -9,59 +9,13 @@
 (require 'init-packages)
 (require 'init-ui)
 (require 'init-better-defaults)
+(require 'init-org)
+(require 'init-keybindings)
+(require 'custom)
 
-
-;;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-;; org模式中注释里语法高亮
-(require 'org)
-(setq org-src-fontify-natively t)
-
-
-
-
-
-
-
-
-
-
-
-
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "C-h C-f") 'counsel-describe-function) ;;查看这个函数
-(global-set-key (kbd "C-h C-v") 'counsel-describe-variable) ;;查看这个变量
-(define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-
-
-
-
-
-
-;; 缩写
- (abbrev-mode t)
-  (define-abbrev-table 'global-abbrev-table '(
-                                              ("8gj" "guojing")
-                                              ))
+;; 
+(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
+(load-file custom-file)
 
 
 
@@ -80,22 +34,33 @@
 
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
 
- '(company-idle-delay 0.08)
- '(company-minimum-prefix-length 1)
- '(custom-safe-themes
-   (quote
-    ("3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default)))
- '(inhibit-startup-screen t)
- '(package-selected-packages (quote (company elpy))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
